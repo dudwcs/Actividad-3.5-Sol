21e8686259ab5fc187fa35e431c4a7462912fc62
dwcs_code_2223 <dwcs.code.2223@gmail.com>
2023-10-18 18:20:00 +0200

    apartado1 commit

---
+++ apartado2/apartado2.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        table,
+        td,
+        th {
+            border: 1px solid black;
+            border-collapse: collapse;
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>A</th>
+                <th>B</th>
+            </tr>
+        </thead>
+    </table>
+</body>
+
+</html>
